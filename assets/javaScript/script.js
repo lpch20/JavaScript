ab4c33810482d82028bfb6059644d805977fe68b
lpch20 <lpch20@hotmail.com>
2022-06-22 22:01:26 -0300

    usando DOM

--- assets/javaScript/script.js
+++ assets/javaScript/script.js
@@ -1,36 +1,89 @@
 /** SEGUNDA ENTREGA, AGREGO DE ARRAYS.
  * El siguiente simulador es en base a una tienda de ropas.
  * Se presenta primeramente el stock disponible.
- * Luego a travers de prompt, se le consulta al usuario que desea comprar. 
- * Luego se le vuelve a consultar si desea agregar algo mas a su compra. 
- * Se hace la suma de lo que el usuario lleva y se le suma el IVA. 
+ * Luego a travers de prompt, se le consulta al usuario que desea comprar.
+ * Luego se le vuelve a consultar si desea agregar algo mas a su compra.
+ * Se hace la suma de lo que el usuario lleva y se le suma el IVA.
  * Por ultimo se le consulta si desea realiza rla compra, si asi lo desea coloca "si" para finalmente efectuarla. De lo contrario, le damos las graciuas por visitarnos en el sitio.
  */
 
-// class Stock {
-//   constructor(nombre, precio, talle) {
-//     this.nombre = nombre;
-//     this.precio = precio;
-//     this.talle = talle;
-//   }
+alert(
+    "BIENVENIDOS A SALIPSER!"
+);
+class Stock {
+  constructor(nombre, precio, talle) {
+    this.nombre = nombre;
+    this.precio = precio;
+    this.talle = talle;
+  }
 
-//   suCompra() {
-//     alert(
-//       `Bienvenidos a Salipser, en este momento contamos con el siguiente stock: ${this.nombre}, al precio de ${this.precio}, en talle ${this.talle}`
-//     );
-//   }
-// }
-// const productos = [];
-// productos.push(new Stock("Remera Ripcurl F", 2500, "L"));
-// productos.push(new Stock("Remera Vans", 1500, "M"));
-// productos.push(new Stock("Pantalon Levis 3500", 4000, "34"));
-// productos.push(new Stock("Pantalon HyM", 2500, "36"));
-// productos.push(new Stock("Campera Vans", 4000, "XL"));
-// productos.push(new Stock("Canguro Levis", 3000, "XL"));
+  suCompra() {
+    alert(
+      "BIENVENIDOS A SALIPSER!"
+    );
+  }
+}
+const productos = [];
+productos.push(new Stock("Remera Ripcurl F", 2500, "L"));
+productos.push(new Stock("Remera Vans", 1500, "M"));
+productos.push(new Stock("Pantalon Levis 3500", 4000, "34"));
+productos.push(new Stock("Pantalon HyM", 2500, "36"));
+productos.push(new Stock("Campera Vans", 4000, "XL"));
+productos.push(new Stock("Canguro Levis", 3000, "XL"));
 
+
+
+const container = document.getElementById("producto1");
+const creacion = document.createElement("div");
+
+creacion.innerHTML = productos[0].nombre;
+
+container.append(creacion);
+
+const container2 = document.getElementById("producto2");
+const creacion2 = document.createElement("div");
+
+creacion2.innerHTML = productos[1].nombre;
+
+container2.append(creacion2);
+
+const container3 = document.getElementById("producto3");
+const creacion3 = document.createElement("div");
+
+creacion3.innerHTML = productos[2].nombre;
+
+container3.append(creacion3);
+
+const container4 = document.getElementById("producto4");
+const creacion4 = document.createElement("div");
+
+creacion4.innerHTML = productos[3].nombre;
+
+container4.append(creacion4);
+
+const container5 = document.getElementById("producto5");
+const creacion5 = document.createElement("div");
+
+creacion5.innerHTML = productos[4].nombre;
+
+container5.append(creacion5);
+
+const container6 = document.getElementById("producto6");
+const creacion6 = document.createElement("div");
+
+creacion6.innerHTML = productos[5].nombre;
+
+container6.append(creacion6);
+
+// productos.forEach((producto) =>{
+//     const li = document.createElement ("li");
+//     li.textContent = productos[0].nombre;
+//     ul.appendChild(li);
+// });
+// producto1.appendChild(ul);
 
 // for (const stock of productos) {
- 
+
 //   stock.suCompra();
 
 // }
@@ -74,7 +127,7 @@
 //       nuevamente = prompt(
 //         "No encontramos el producto que quiere, intente nuevamente"
 //       );
-      
+
 //   }
 
 //   const otroProducto = prompt("¿Desea agregar otro producto a su compra?");
@@ -97,7 +150,7 @@
 //       alert("El precio es de " + "$ " + resultado + " mas IVA incluido.");
 //       break;
 //     case "Campera vans":
-//       resultado2 = productos[4].precio * 1.22;  
+//       resultado2 = productos[4].precio * 1.22;
 //       alert("El precio es de " + "$ " + resultado + " mas IVA incluido.");
 //       break;
 //     case "Canguro Levis":
@@ -127,5 +180,3 @@
 // }
 
 // compraFinal();
-
-
