5d118a16d5fc3a4afbfb125b8456f1ea83a06c46
lpch20 <lpch20@hotmail.com>
2022-07-31 21:36:43 -0300

    Finalizando el proyecto

--- assets/javaScript/carrito.js
+++ assets/javaScript/carrito.js
@@ -4,68 +4,82 @@ class Carrito {
         e.preventDefault();
         if (e.target.classList.contains("compra")) {
             //si se hace click en el boton "compra" de html.......
-            const producto = e.target.parentElement.parentElement; //se guardan los productos.
-            this.datoProducto(producto);
+            const productosHtml = e.target.parentElement.parentElement; //se guardan los productos.
+            this.datoProducto(productosHtml);
         }
     }
 
-    datoProducto(producto) {
+    datoProducto(productosHtml) { //seleccion de datos del html.
         const detallesProducto = {
-            imagen: producto.querySelector("img").src,
-            nombre: producto.querySelector("h4").textContent,
-            precio: producto.querySelector(".costo span").textContent,
-            id: producto.querySelector("a").getAttribute("data-id"),
+            imagen: productosHtml.querySelector("img").src,
+            nombre: productosHtml.querySelector("h4").textContent,
+            precio: productosHtml.querySelector(".costo span").textContent,
+            id: productosHtml.querySelector("a").getAttribute("data-id"),
             cantidad: 1,
         };
 
         this.productoEnCarrito(detallesProducto);
     }
 
-    productoEnCarrito(producto) {
+    productoEnCarrito(productosHtml) { // creacion de las columnas en el carrito.
         const row = document.createElement("tr");
         row.innerHTML = `
         <td>
-            <img src="${producto.imagen}" width=100>
+            <img src="${productosHtml.imagen}" width=70 height=70>
         </td>
         <td>
-            ${producto.nombre}
+            ${productosHtml.nombre}
         </td>
         <td>
-            ${producto.precio}
+            ${productosHtml.precio}
         </td>
         <td>
-            <a href="#" class="borrar-producto fas fa-times-circle" data-id=" ${producto.id}"></a>
+            <a href="#" class="borrar-producto fas fa-times-circle" data-id="${productosHtml.id}"></a>
         </td>
         `;
         productosAgregados.appendChild(row);
-        this.guardarProductoLocalStorage(producto);
+        this.guardarProductoLocalStorage(productosHtml);
     }
 
-    vaciarCarrito(e) {
+    eliminarProducto(e) { // eliminar el producto de html.
+        e.preventDefault();
+        let producto, productoID;
+        if (e.target.classList.contains("borrar-producto")) {
+            e.target.parentElement.parentElement.remove();
+            producto = e.target.parentElement.parentElement;
+            productoID = producto.querySelector("a").getAttribute("data-id");
+        }
+
+        this.eliminarProductoLocalStorage(productoID);
+    }
+
+    vaciarCarrito(e) { // vaciar el carrito, conj un sweet alert para su confirmacion. 
         e.preventDefault();
         Swal.fire({
-            title: '¿Estas seguro/a que deseas eliminar el producto?',
-            icon: 'question',
-            iconHtml: '؟',
-            confirmButtonText: 'Si',
-            cancelButtonText: 'No',
+            title: "¿Estas seguro/a que deseas eliminar el producto?",
+            icon: "question",
+            iconHtml: "؟",
+            confirmButtonText: "Si",
+            cancelButtonText: "No",
             showCancelButton: true,
-            showCloseButton: false, 
-        })
+            showCloseButton: false,
+        });
         while (productosAgregados.firstChild) {
             productosAgregados.removeChild(productosAgregados.firstChild);
         }
+        this.eliminarLocalStorage();
         return false;
+
     }
 
-    guardarProductoLocalStorage(prdocuto) {
+    guardarProductoLocalStorage(productosHtml) { // guardar el producto en el local storage.
         let productos;
         productos = this.obtenerProductos();
-        productos.push("producto");
+        productos.push(productosHtml);
         localStorage.setItem("productos", JSON.stringify(productos));
     }
 
-    obtenerProductos() {
+    obtenerProductos() { // obtener el producto del local storage para saber si ya se encuentra ahi y asi poder luego eliminarlo. 
         let productosLocalStorage;
 
         if (localStorage.getItem("productos") === null) {
@@ -76,16 +90,52 @@ class Carrito {
         return productosLocalStorage;
     }
 
+    eliminarProductoLocalStorage(productoID) { // eliminar el producto del localstorage.
+        let productosLS;
+        productosLS = this.obtenerProductos();
+        productosLS.forEach((productosLocalStorage, index) => {
+            if (productosLocalStorage.id === productoID) {
+                productosLS.splice(index, 1);
+            }
+        });
 
+        localStorage.setItem("productos", JSON.stringify(productosLS));
+    }
+
+    obternerLocalSotrage() {  // cuadno se recargue la pagina, mantener los productos en el local y asi en el carrito. 
+        let productosLS;
+        productosLS = this.obtenerProductos();
+        productosLS.forEach(function (productosHtml) {
+            const row = document.createElement("tr");
+            row.innerHTML = `
+        <td>
+            <img src="${productosHtml.imagen}" width=70 height=70>
+        </td>
+        <td>
+            ${productosHtml.nombre}
+        </td>
+        <td>
+            ${productosHtml.precio}
+        </td>
+        <td>
+            <a href="#" class="borrar-producto fas fa-times-circle" data-id="${productosHtml.id}"></a>
+        </td>
+        `;
+            productosAgregados.appendChild(row);
+        });
+    }
+
+    eliminarLocalStorage(){ 
+        localStorage.clear();
+    }
 }
 
-fetch('../../elements.json')
-.then(respueta => {
-    return respueta.json()
-})
-.then(datos =>{
-    datos.forEach((producto) => {
-        console.log(datos[0].nombre)
-        
+fetch("../../elements.json") // Se utiliza JSON para la subida de los productos a la venta. 
+    .then((respueta) => {
+        return respueta.json();
+    })
+    .then((datos) => {
+        datos.forEach(function () {
+            console.log(datos.nombre);
+        });
     });
-})
--- assets/javaScript/variablesCarrito.js
+++ assets/javaScript/variablesCarrito.js
@@ -5,8 +5,10 @@ const productosAgregados = document.querySelector('#lista-carrito tbody')
 const vaciarCarritoBtn = document.getElementById('vaciar-carrito');
 
 function agregarEventos(){
-    productos.addEventListener('click', (e) => { carro.comprarProducto(e); }); //cuando se hace click en el catalogo de productos, se ejecuta el meteodo de comprar producto.
-    vaciarCarritoBtn.addEventListener('click', (e)=> {carro.vaciarCarrito(e)});
+    productos.addEventListener('click', (e) => {carro.comprarProducto(e);}); //cuando se hace click en el catalogo de productos, se ejecuta el meteodo de comprar producto.
+    vaciarCarritoBtn.addEventListener('click', (e)=> {carro.vaciarCarrito(e);});
+    carrito.addEventListener('click', (e)=> {carro.eliminarProducto(e);});
+    document.addEventListener('DOMContentLoaded', carro.obternerLocalSotrage());
 }
 
 agregarEventos();
